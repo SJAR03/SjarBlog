02b43dc71ee2aab0b698dbd691f2e827f271f222
Sergio Ayerdis <72574282+SJAR03@users.noreply.github.com>
2022-11-08 20:21:38 -0600

    fix config

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -9,7 +9,7 @@ const PostCard = ({ post }) => {
   return (
     <div className='bg-white shadow-lg rounded-lg p-0 lg:p-8 pb-12 mb-8'>
       <div className='relative overflow-hidden shadow-md pb-80 mb-6'>
-        <Image
+        <img
           src={post.featuredImage.url}
           alt={post.title}
           className="object-top absolute h-80 w-full object-fill shadow-lg ronded-t-lg lg:rounded-lg" 
